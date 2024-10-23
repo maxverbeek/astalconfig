22e6aecbd86495821f3e651f01c2c8e733704468
Max Verbeek <m4xv3rb33k@gmail.com>
2024-10-23 23:21:57 +0200

    feat: add a bar per monitor

--- app.ts
+++ app.ts
@@ -1,11 +1,23 @@
-import { App } from "astal"
+import { App, Gdk, Gtk } from "astal"
 import style from "inline:./style.css"
 import Bar from "./widget/Bar"
 
 App.start({
   css: style,
   main() {
-    Bar(0)
-    // Bar(1) // initialize other monitors
+    const bars = new Map<Gdk.Monitor, Gtk.Widget>()
+
+    for (const gdkmonitor of App.get_monitors()) {
+      bars.set(gdkmonitor, Bar(gdkmonitor))
+    }
+
+    App.connect("monitor-added", (_, gdkmonitor) => {
+      bars.set(gdkmonitor, Bar(gdkmonitor))
+    })
+
+    App.connect("monitor-removed", (_, gdkmonitor) => {
+      bars.get(gdkmonitor)?.destroy()
+      bars.delete(gdkmonitor)
+    })
   },
 })
--- widget/Bar.tsx
+++ widget/Bar.tsx
@@ -1,4 +1,4 @@
-import { App, Variable, Astal, Gtk, bind } from "astal"
+import { App, Variable, Astal, Gtk, bind, Gdk } from "astal"
 import Niri, { Window } from "../service/niri"
 
 const niri = new Niri()
@@ -8,10 +8,23 @@ function appnames(windows: Window[]) {
   return windows.map(w => w.title?.endsWith('NVIM') ? 'neovim BTW' : w.app_id).join(', ')
 }
 
-function Workspaces() {
+// bruh
+const manufacturer2output: Record<string, string> = {
+  'Dell Inc.': 'DP-3',
+  'PNP(AOC)': 'DP-2'
+}
+
+function Workspaces({ onlyForOutput }: { onlyForOutput: string }) {
+  const nirioutput = manufacturer2output[onlyForOutput]
+
+  if (!nirioutput) {
+    console.error(`couldn't find output for model ${onlyForOutput}`)
+    return <box>no output</box>
+  }
+
   return <box>
     {bind(niri, 'outputs')
-      .as(os => Object.values(os).flatMap((o) => Object.values(o.workspaces))
+      .as(os => Object.values(os).filter(os => os.output === nirioutput).flatMap((o) => Object.values(o.workspaces))
         .map(ws => {
           const text = ws.is_active ? `${ws.idx} (${appnames(ws.windows)})` : `${ws.idx}`
           return <button>{text}</button>
@@ -19,15 +32,17 @@ function Workspaces() {
   </box>
 }
 
-export default function Bar(monitor: number) {
+export default function Bar(monitor: Gdk.Monitor) {
   return <window
     className="Bar"
-    monitor={monitor}
+    gdkmonitor={monitor}
     exclusivity={Astal.Exclusivity.EXCLUSIVE}
     anchor={Astal.WindowAnchor.TOP
       | Astal.WindowAnchor.LEFT
       | Astal.WindowAnchor.RIGHT}
     application={App}>
-    <Workspaces></Workspaces>
+    <box>
+      <Workspaces onlyForOutput={monitor.manufacturer}></Workspaces>
+    </box>
   </window>
 }
