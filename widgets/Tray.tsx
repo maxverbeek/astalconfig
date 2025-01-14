7cae142f8ea3260844c010e150d0fc0af0583495
Max Verbeek <m4xv3rb33k@gmail.com>
2025-01-14 19:10:59 +0100

    feat: update

--- widgets/Tray.tsx
+++ widgets/Tray.tsx
@@ -1,5 +1,4 @@
 import AstalTray from "gi://AstalTray?version=0.1";
-import { Astal, Gdk, Gtk } from "astal/gtk3";
 import { bind } from "astal";
 
 export default function Tray() {
@@ -7,18 +6,13 @@ export default function Tray() {
 
   return <box className="Tray">
     {bind(tray, "items").as(items => items.map(item => {
-      const menu = item.create_menu()
-      return <button
-        tooltip_markup={bind(item, "tooltip_markup")}
-        valign={Gtk.Align.CENTER}
-        halign={Gtk.Align.CENTER}
-        onClick={(self) => {
-          menu?.popup_at_widget(self, Gdk.Gravity.SOUTH, Gdk.Gravity.NORTH, null)
-        }}
-        onDestroy={() => menu?.destroy()}
-      >
-        <icon g_icon={bind(item, "gicon")} />
-      </button>
+      return <menubutton
+        tooltipMarkup={bind(item, "tooltipMarkup")}
+        usePopover={false}
+        actionGroup={bind(item, "actionGroup").as(ag => ["dbusmenu", ag])}
+        menuModel={bind(item, "menuModel")}>
+        <icon gicon={bind(item, "gicon")} />
+      </menubutton>
     }))}
   </box>
 }
