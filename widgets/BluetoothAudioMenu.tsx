3a52805fd6b97d8c5c5e1c2e58bcbd955a3a671d
Max Verbeek <m4xv3rb33k@gmail.com>
2025-10-07 23:24:43 +0200

    feat: add some more bt audio stuff

--- widgets/BluetoothAudioMenu.tsx
+++ widgets/BluetoothAudioMenu.tsx
@@ -1,9 +1,63 @@
 import AstalBluetooth from 'gi://AstalBluetooth?version=0.1'
 import Wireplumber from 'gi://AstalWp'
-import { createBinding } from 'gnim'
+import Gtk from 'gi://Gtk?version=4.0'
+import { createBinding, For } from 'gnim'
+
+const { HORIZONTAL, VERTICAL } = Gtk.Orientation
 
 function percentage(perc: number): string {
   return `${Math.floor(perc * 100)}%`
+}
+
+type AudioSliderProps = {
+  endpoint: Wireplumber.Endpoint
+}
+
+function AudioSlider({ endpoint }: AudioSliderProps) {
+  const vol = createBinding(endpoint, "volume")
+  const volpct = vol(percentage)
+  return <box>
+    <button onClicked={() => endpoint.mute = !endpoint.mute}>
+      <box>
+        <image icon_name={createBinding(endpoint, "volume_icon")} />
+        <label label={volpct} />
+      </box>
+    </button>
+    <slider
+      widthRequest={260}
+      onChangeValue={({ value }) => endpoint.set_volume(value)}
+      value={createBinding(endpoint, "volume")}
+    />
+  </box>
+}
+
+function BluetoothDevices() {
+  const bt = AstalBluetooth.get_default()
+
+  const visible = createBinding(bt, "adapters")(adapters => adapters.length > 0)
+  const devices = createBinding(bt, "devices")
+
+  return <box orientation={VERTICAL} visible={visible}>
+    <For each={devices}>
+      {device => <box>
+        <button
+          onClicked={() => device.connect_device((result) => console.log("connect device", result))}
+          hexpand
+        >
+          <box>
+            <image icon_name={createBinding(device, "icon")} />
+            <label label={createBinding(device, "name")} />
+            <Gtk.Spinner visible={createBinding(device, "connecting")} />
+          </box>
+        </button>
+        <button
+          visible={createBinding(device, "connected")}
+          icon_name="window-close-symbolic"
+          onClicked={() => device.disconnect_device((result) => console.log("disconnect device", result))}
+        />
+      </box>}
+    </For>
+  </box>
 }
 
 function BluetoothAudioMenu() {
@@ -26,6 +80,11 @@ function BluetoothAudioMenu() {
       <image tooltip_text={microphoneVolume} icon_name={microphoneIcon} />
     </box>
     <popover>
+      <box orientation={VERTICAL}>
+        <AudioSlider endpoint={defaultSpeaker} />
+        <AudioSlider endpoint={defaultMicrophone} />
+        <BluetoothDevices />
+      </box>
     </popover>
   </menubutton>
 }
