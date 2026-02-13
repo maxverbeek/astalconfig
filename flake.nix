43e21ebc87eaad4dda529d70f43c5ed6f6d395fa
Max Verbeek <m4xv3rb33k@gmail.com>
2026-02-13 18:43:19 +0100

    build: fix flake

--- flake.nix
+++ flake.nix
@@ -25,7 +25,7 @@
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
       pname = "my-shell";
-      entry = "app.ts";
+      entry = "app.tsx";
 
       astalPackages = with ags.packages.${system}; [
         io
